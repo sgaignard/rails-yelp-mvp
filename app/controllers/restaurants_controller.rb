a2d87a4f7e683984d65f1017531a10d77afd06c8
shane gaignard <shane.c.gaignard@gmail.com>
2022-05-18 12:10:26 +0200

    finished models

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def restaurant_params
+  end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, :inclusion => 0..5
+  validates :rating, numericality: { only_integer: true }
+  validates :content, presence: true
+end
